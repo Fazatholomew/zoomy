0ad39877a2f77fc900b4718e7c0986fa9c8b2e77
Faza Byandika Hikmatullah <thereverend.sullivan@yahoo.co.id>
2021-03-05 22:25:32 -0500

    Init commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var url = 'https://zoomy.jimmyganteng.com/';
+const move = async (direction) => {
+  const sensitivity = document.getElementById('sensitivity').value;
+  fetch(`${url}${direction}?sensitivity=${sensitivity}`);
+};
+
+const speak = async () => {
+  const text = document.getElementById('speak').value;
+  fetch(`${url}speak?text=${text}`);
+};
+
+const updateSensitivity = (value) => {
+  const sensitivityElement = document.getElementById('value');
+  sensitivityElement.innerText = value; 
+};
+
+const checkServer = async () => {
+  fetch(`${url}password?password=${password}`)
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        const serverIndicatorEl = document.getElementById('server');
+        serverIndicatorEl.innerText = 'Server is online.'
+      }
+    }); 
+};
+
+checkServer();
